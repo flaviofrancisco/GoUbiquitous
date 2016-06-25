7ba4470b7cc58ac026ad24cda735cc684bdc08c8
Flavio <flaviofrancis@msn.com>
2016-06-25 13:09:14 +0200

    Base Go Ubiquitous Watch Project

--- wear/src/main/java/com/derhaifisch/wear/GoUbiquitousWatchFaceService.java
+++ wear/src/main/java/com/derhaifisch/wear/GoUbiquitousWatchFaceService.java
@@ -3,20 +3,53 @@ package com.derhaifisch.wear;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Picture;
+import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.wearable.view.ConfirmationOverlay;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
 import android.text.format.Time;
 import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.Wearable;
 
 import java.security.PrivateKey;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.StringTokenizer;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
@@ -34,28 +67,25 @@ public class GoUbiquitousWatchFaceService extends CanvasWatchFaceService {
 
     private class GoUbiquitousWatchFaceEngine extends CanvasWatchFaceService.Engine {
 
-        static final int BORDER_WIDTH_PX = 5;
-
         final Rect mCardBounds = new Rect();
-        final Paint mPaint = new Paint();
 
-        private Typeface WATCH_TEXT_TYPEFACE = Typeface.create(Typeface.SERIF, Typeface.NORMAL);
+        private Typeface WATCH_TEXT_TYPEFACE = Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD);
+        private Typeface WATCH_TEXT_DATE = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL);
+
         private  static final int MSG_UPDATE_TIME_ID = 42;
         private long mUpdateRateMs = 1000;
         private Time mDisplayTime;
 
         private Paint mBackgroundColorPaint;
         private Paint mTextColorPaint;
+        private Paint mDatePaint;
 
         private boolean mHasTimeZoneReceiverBeenRegistered = false;
         private boolean mIsMuteMode;
         private  boolean mIsLowBitAmbient;
 
-        private float mXOffset;
-        private float mYOffset;
-
-        private int mBackgroundColor = Color.parseColor("black");
-        private int mTextColor = Color.parseColor("red");
+        private int mBackgroundColor = Color.parseColor("#03A9F4");
+        private int mTextColor = Color.parseColor("#FFFFFF");
 
         final BroadcastReceiver mTimerZoneBroadCastReceiver = new BroadcastReceiver() {
             @Override
@@ -91,6 +121,7 @@ public class GoUbiquitousWatchFaceService extends CanvasWatchFaceService {
                 Log.d(TAG, "onCreate");
             }
             super.onCreate(holder);
+
             setWatchFaceStyle(new WatchFaceStyle.Builder(GoUbiquitousWatchFaceService.this)
                     .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
                     .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
@@ -99,8 +130,10 @@ public class GoUbiquitousWatchFaceService extends CanvasWatchFaceService {
                     .build());
 
             mDisplayTime = new Time();
+
             initBackground();
             initDisplayText();
+            initDisplayDate();
         }
 
         private  void initBackground()
@@ -114,15 +147,109 @@ public class GoUbiquitousWatchFaceService extends CanvasWatchFaceService {
             mTextColorPaint.setColor(mTextColor);
             mTextColorPaint.setTypeface(WATCH_TEXT_TYPEFACE);
             mTextColorPaint.setAntiAlias(true);
-            mTextColorPaint.setTextSize(32);
+            mTextColorPaint.setTextSize(64);
+        }
+
+        private  void initDisplayDate(){
+            mDatePaint = new Paint();
+            mDatePaint.setColor(mTextColor);
+            mDatePaint.setTypeface(WATCH_TEXT_DATE);
+            mDatePaint.setAntiAlias(true);
+            mDatePaint.setTextSize(32);
         }
 
         @Override
-        public void onAmbientModeChanged(boolean inAmbientMode) {
-            if (Log.isLoggable(TAG, Log.DEBUG)) {
-                Log.d(TAG, "onAmbientModeChanged: " + inAmbientMode);
+        public void onVisibilityChanged(boolean visible){
+            super.onVisibilityChanged(visible);
+
+            if(visible){
+                if(!mHasTimeZoneReceiverBeenRegistered){
+                    IntentFilter filter = new IntentFilter(Intent.ACTION_TIME_CHANGED);
+                    GoUbiquitousWatchFaceService.this.registerReceiver(mTimerZoneBroadCastReceiver, filter);
+                    mHasTimeZoneReceiverBeenRegistered = true;
+                }
+
+                mDisplayTime.clear(TimeZone.getDefault().getID());
+                mDisplayTime.setToNow();
+
+            } else {
+                if(mHasTimeZoneReceiverBeenRegistered){
+                    GoUbiquitousWatchFaceService.this.unregisterReceiver(mTimerZoneBroadCastReceiver);
+                    mHasTimeZoneReceiverBeenRegistered = false;
+                }
             }
+
+            updateTimer();
+        }
+
+        private  void updateTimer(){
+            mTimeHandler.removeMessages(MSG_UPDATE_TIME_ID);
+            if(isVisible() && !isInAmbientMode())
+            {
+                mTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME_ID);
+            }
+        }
+
+        @Override
+        public void onApplyWindowInsets(WindowInsets insets){
+            super.onApplyWindowInsets(insets);
+        }
+
+        @Override
+        public void onPropertiesChanged(Bundle properties){
+            super.onPropertiesChanged(properties);
+
+            if(properties.getBoolean(PROPERTY_BURN_IN_PROTECTION, false)){
+                mIsLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
+            }
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode){
             super.onAmbientModeChanged(inAmbientMode);
+
+            if(inAmbientMode){
+                mTextColorPaint.setColor(Color.parseColor("white"));
+            } else {
+                mTextColorPaint.setColor(Color.parseColor("red"));
+            }
+
+            if(mIsLowBitAmbient){
+                mTextColorPaint.setAntiAlias(!inAmbientMode);
+            }
+
+            invalidate();
+            updateTimer();
+        }
+
+        @Override
+        public void onInterruptionFilterChanged(int interruptionFilter){
+            super.onInterruptionFilterChanged(interruptionFilter);
+
+            boolean isDeviceMuted = (interruptionFilter ==
+                    WatchFaceService.INTERRUPTION_FILTER_NONE);
+
+            if(isDeviceMuted){
+             mUpdateRateMs = TimeUnit.MINUTES.toMillis(1);
+            } else {
+                mUpdateRateMs = 1000;
+            }
+
+            if(mIsMuteMode != isDeviceMuted){
+                mIsMuteMode = isDeviceMuted;
+                int alpha = (isDeviceMuted) ? 100 : 255;
+                mTextColorPaint.setAlpha(alpha);
+                invalidate();
+                updateTimer();
+            }
+        }
+
+        @Override
+        public void onTimeTick(){
+            super.onTimeTick();
+            if(Log.isLoggable(TAG, Log.DEBUG)){
+                Log.d(TAG, "onTimeTick: ambient = " + isInAmbientMode());
+            }
             invalidate();
         }
 
@@ -141,29 +268,93 @@ public class GoUbiquitousWatchFaceService extends CanvasWatchFaceService {
 
         @Override
         public void onDraw(Canvas canvas, Rect bounds) {
-            // Clear screen.
-            canvas.drawColor(isInAmbientMode() ? Color.BLACK : Color.GREEN);
+            super.onDraw(canvas, bounds);
 
-            // Draw border around card in interactive mode.
-            if (!isInAmbientMode()) {
-                mPaint.setColor(Color.MAGENTA);
-                canvas.drawRect(mCardBounds.left - BORDER_WIDTH_PX,
-                        mCardBounds.top - BORDER_WIDTH_PX,
-                        mCardBounds.right + BORDER_WIDTH_PX,
-                        mCardBounds.bottom + BORDER_WIDTH_PX, mPaint);
-            }
+            mDisplayTime.setToNow();
 
-            // Fill area under card.
-            mPaint.setColor(isInAmbientMode() ? Color.RED : Color.GREEN);
-            canvas.drawRect(mCardBounds, mPaint);
+            drawBackground(canvas, bounds);
+            drawText(canvas, bounds);
+            drawWeatherImage(canvas, bounds);
         }
 
-        @Override
-        public void onTimeTick(){
-            super.onTimeTick();
-            if(Log.isLoggable(TAG, Log.DEBUG)){
-                Log.d(TAG, "onTimeTick: ambient = " + isInAmbientMode());
+        private void drawBackground(Canvas canvas, Rect bounds){
+            canvas.drawRect(0,0, bounds.width(), bounds.height(), mBackgroundColorPaint);
+        }
+
+        private void drawText(Canvas canvas, Rect bounds){
+
+            String timeText = getHoursString() + ":" + String.format("%02d", mDisplayTime.minute);
+
+            if(isInAmbientMode() || mIsMuteMode){
+             timeText += (mDisplayTime.hour < 12) ? "AM" : "PM";
+            } else {
+                timeText += ":" + String.format("%02d", mDisplayTime.second);
             }
+
+            float timeXOffset = computeXOffset(timeText, mTextColorPaint, bounds);
+            float timeYOffset = computeTimeYOffset(timeText, mTextColorPaint, bounds);
+
+            canvas.drawText(timeText, timeXOffset , timeYOffset , mTextColorPaint);
+
+            //Date of the day
+            drawTextLine(canvas, bounds, mDatePaint, GetCurrentDate(), 40f);
+
+            //Temperature
+            drawTextLine(canvas, bounds, mDatePaint, "20ºC | 35ºC", 100f);
+        }
+
+        private void drawTextLine(Canvas canvas, Rect bounds, Paint paint, String textContent, float top)
+        {
+            float timeXOffset = computeXOffset(textContent, paint, bounds);
+            float timeYOffset = computeTimeYOffset(textContent, paint, bounds);
+            canvas.drawText(textContent, timeXOffset , timeYOffset + top , paint);
+        }
+
+        private void drawWeatherImage(Canvas canvas, Rect bounds) {
+            Bitmap weatherPic = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.art_clear);
+
+            int picResize = weatherPic.getWidth() / 2;
+            Bitmap weatherPicResized = Bitmap.createScaledBitmap(weatherPic, picResize, picResize, true);
+
+            int centerX = (Math.round(bounds.exactCenterX()) - 1) - (picResize/ 2);
+
+            canvas.drawBitmap(weatherPicResized, centerX, 40, null);
+        }
+
+        private String getHoursString(){
+            if(mDisplayTime.hour % 12 ==0)
+                return "12";
+            else if (mDisplayTime.hour <= 12)
+                return String.valueOf(mDisplayTime.hour);
+            else
+                return String.valueOf(mDisplayTime.hour -12);
+        }
+
+
+        private float computeXOffset(String text, Paint paint, Rect watchBounds) {
+            float centerX = watchBounds.exactCenterX();
+            float timeLength = paint.measureText(text);
+            return centerX - (timeLength / 2.0f);
+        }
+
+        private float computeTimeYOffset(String timeText, Paint timePaint, Rect watchBounds) {
+            float centerY = watchBounds.exactCenterY();
+            Rect textBounds = new Rect();
+            timePaint.getTextBounds(timeText, 0, timeText.length(), textBounds);
+            int textHeight = textBounds.height();
+            return centerY + (textHeight / 2.0f);
+        }
+
+        private String GetCurrentDate()
+        {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.YEAR, calendar.getTime().getYear());
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.getTime().getDay());
+            calendar.set(Calendar.MONTH, calendar.getTime().getMonth());
+            String format = new SimpleDateFormat("EEE, MMM d, ''yy").format(calendar.getTime());
+            return format;
         }
     }
 }
+
